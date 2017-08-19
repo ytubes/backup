1e369766e8ecccbe988d9d397ec4c69d4465fcc7
ytubes <adx.informer@gmail.com>
2017-08-19 21:05:39 +0700

    Update DumpManager.php

--- components/base/DumpManager.php
+++ components/base/DumpManager.php
@@ -14,108 +14,108 @@ use ytubes\backup\Module;
  */
 abstract class DumpManager extends \yii\base\Component
 {
-	/**
-	 * @var string Директория бекапов сайта.
-	 */
-	protected $backupDirectory;
-	/**
-	 * Процедура создания бекапа. Дампит базу в файл.
-	 * @param string $dumpFile файл дампа базы.
-	 * @return void
-	 */
-	abstract public function procedureBackup();
-	/**
-	 * Восстанавливает базу из бекапа.
-	 * @param string $dumpFile файл дампа базы.
-	 * @return void
-	 */
-	abstract public function procedureRestore($dumpFilename);
-	/**
-	 * Удаляет старые файлы. Если количество файлов превышает 7 штук, а срок более недели - удаляем.
-	 *
-	 * @return void
-	 */
-	public function deleteOldDumps()
-	{
-		$weekAgoTime = time() - (60 * 60 * 24 * 7);
+    /**
+     * @var string Директория бекапов сайта.
+     */
+    protected $backupDirectory;
+    /**
+     * Процедура создания бекапа. Дампит базу в файл.
+     * @param string $dumpFile файл дампа базы.
+     * @return void
+     */
+    abstract public function procedureBackup();
+    /**
+     * Восстанавливает базу из бекапа.
+     * @param string $dumpFile файл дампа базы.
+     * @return void
+     */
+    abstract public function procedureRestore($dumpFilename);
+    /**
+     * Удаляет старые файлы. Если количество файлов превышает 7 штук, а срок более недели - удаляем.
+     *
+     * @return void
+     */
+    public function deleteOldDumps()
+    {
+        $weekAgoTime = time() - (60 * 60 * 24 * 7);
 
-		$files = $this->getDumpFiles();
+        $files = $this->getDumpFiles();
 
-		$deletedCounter = 0; // счетчик удаленных файлов
-		$filesCounter = 0; // счетчик файлов
-		foreach ($files as $file) {
-	        if ($filesCounter >= 7 && $file->getMTime() > $weekAgoTime) {
-	        	if (false === @unlink($file->getPathname())) {
-	        		// тут лог написать что не удалось удалить.
-	        		continue;
-	        	}
+        $deletedCounter = 0; // счетчик удаленных файлов
 
-	        	$deletedCounter ++;
-	        }
+        foreach ($files as $file) {
+            $fileMTime = filemtime($file->getPathname());
 
-	        $filesCounter++;
-		}
+            if ($fileMTime < $weekAgoTime) {
+                if (false === @unlink($file->getPathname())) {
+                    // тут лог написать что не удалось удалить.
+                    continue;
+                }
 
-		return $deletedCounter;
-	}
-	/**
-	 * Генерирует путь к файлу дампа. Его имя и создает директории к пути, если необходимо.
-	 * @param string $suffix название файла (суфикс к дате)
-	 * @param string $extension расширение будущего дампа
-	 * @return string
-	 */
-	protected function generateDumpFilepath($suffix = '', $extension = 'txt')
-	{
-		$date = gmdate('Y-m-d');
+                $deletedCounter ++;
+            }
+        }
 
-		if (!is_dir($this->backupDirectory)) {
-			FileHelper::createDirectory($this->backupDirectory, 0755);
-		}
+        return $deletedCounter;
+    }
+    /**
+     * Генерирует путь к файлу дампа. Его имя и создает директории к пути, если необходимо.
+     * @param string $suffix название файла (суфикс к дате)
+     * @param string $extension расширение будущего дампа
+     * @return string
+     */
+    protected function generateDumpFilepath($suffix = '', $extension = 'txt')
+    {
+        $date = gmdate('Y-m-d');
 
-		$security = new Security();
-		$randString = $security->generateRandomString(8);
-		$dumpFilename = $date . '_' . (!empty($suffix) ? $suffix . '_' : '') . $randString . '.' . $extension;
+        if (!is_dir($this->backupDirectory)) {
+            FileHelper::createDirectory($this->backupDirectory, 0755);
+        }
 
-		return $this->backupDirectory . DIRECTORY_SEPARATOR . $dumpFilename;
-	}
-	/**
-	 * Получает список файлов из директории бекапа.
-	 * @return array
-	 */
-	public function getDumpFiles()
-	{
-		if (!is_dir($this->backupDirectory)) {
-			return [];
-		}
+        $security = new Security();
+        $randString = $security->generateRandomString(8);
+        $dumpFilename = $date . '_' . (!empty($suffix) ? $suffix . '_' : '') . $randString . '.' . $extension;
 
-			// Сортирует по времени модификации в обратном порядке
-		$sort = function ($a, $b) {
+        return $this->backupDirectory . DIRECTORY_SEPARATOR . $dumpFilename;
+    }
+    /**
+     * Получает список файлов из директории бекапа.
+     * @return array
+     */
+    public function getDumpFiles()
+    {
+        if (!is_dir($this->backupDirectory)) {
+            return [];
+        }
+
+            // Сортирует по времени модификации в обратном порядке
+        $sort = function ($a, $b) {
             return $b->getMTime() - $a->getMTime();
         };
 
-		$finder = new Finder();
-		$finder->files()
-			->ignoreDotFiles(true)
-			->in($this->backupDirectory)
-			->sort($sort)
-			->depth('== 0');
+        $finder = new Finder();
+        $finder->files()
+            ->ignoreDotFiles(true)
+            ->in($this->backupDirectory)
+            ->sort($sort)
+            ->depth('== 0');
 
-		return $finder;
-	}
-	/**
-	 * Получение файла бекапа по его имени в директории бекапа.
-	 * @param string $filename
-	 * @return object \SplFileInfo
-	 */
-	public function getFile($filename)
-	{
-		$filepath = $this->backupDirectory . DIRECTORY_SEPARATOR . $filename;
-		$file = new \SplFileInfo($filepath);
+        return $finder;
+    }
+    /**
+     * Получение файла бекапа по его имени в директории бекапа.
+     * @param string $filename
+     * @return object \SplFileInfo
+     */
+    public function getFile($filename)
+    {
+        $filepath = $this->backupDirectory . DIRECTORY_SEPARATOR . $filename;
+        $file = new \SplFileInfo($filepath);
 
-		if (!$file->isFile()) {
-			throw new InvalidConfigException("File {$filename} not exists");
-		}
+        if (!$file->isFile()) {
+            throw new InvalidConfigException("File {$filename} not exists");
+        }
 
-		return $file;
-	}
+        return $file;
+    }
 }
